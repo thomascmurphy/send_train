8a3eed94c71b0d43527e0767dc7ef57265d4e7d3
Thomas Murphy <thomas.c.murphy@gmail.com>
2016-03-10 11:41:20 -0700

    better coach stuff for postgres

--- db/migrate/20160309143304_create_user_coaches.rb
+++ db/migrate/20160309143304_create_user_coaches.rb
@@ -2,7 +2,7 @@ class CreateUserCoaches < ActiveRecord::Migration
   def change
     create_table :user_coaches do |t|
       t.references :user, index: true, foreign_key: true
-      t.references :coach, index: true, foreign_key: true
+      t.integer :coach_id, index: true
       t.timestamps null: false
     end
   end
