b1d594a7447dadd4652db6c08b31d1695fdffdd0
Thomas Murphy <thomas.c.murphy@gmail.com>
2016-04-20 12:18:20 -0600

    ability to edit/delete attempts, new climbs slide

--- app/controllers/attempts_controller.rb
+++ app/controllers/attempts_controller.rb
@@ -1,5 +1,12 @@
 class AttemptsController < ApplicationController
 
+  def set_other_models
+    @boulder_grades = Climb.bouldering_grades(current_user.grade_format)
+    @sport_grades = Climb.sport_grades(current_user.grade_format)
+    @climbs = current_user.climbs.order(created_at: :desc)
+    @climbs = @climbs.sort_by{|c| [c.redpointed ? 0 : 1, c.redpoint_date]}.reverse
+  end
+
   def index
 
   end
@@ -20,7 +27,6 @@ class AttemptsController < ApplicationController
   end
 
   def create
-    @climbs = current_user.climbs.order(created_at: :desc)
     @climb = current_user.climbs.find_by_id(params[:climb_id])
     date = params[:date]
     if date.present?
@@ -33,7 +39,7 @@ class AttemptsController < ApplicationController
 
     respond_to do |format|
       if @attempt.save
-        @climbs = @climbs.sort_by{|c| [c.redpointed ? 0 : 1, c.redpoint_date]}.reverse
+        self.set_other_models()
         format.html { redirect_to @attempt, notice: 'Attempt was successfully created.' }
         format.js
         format.json { render json: @attempt, status: :created, location: @attempt }
@@ -45,11 +51,37 @@ class AttemptsController < ApplicationController
   end
 
   def edit
-
+    @attempt = current_user.attempts.find_by_id(params[:id])
+    @climb = @attempt.climb
+    respond_to do |format|
+      format.html
+      format.js
+      format.json { render json: @attempt, status: :ok, location: @attempt }
+    end
   end
 
   def update
+    @attempt = current_user.attempts.find_by_id(params[:id])
+    @climb = @attempt.climb
+    date = params[:date]
+    if date.present?
+      if date[:day].present? && date[:month].present? && date[:year].present?
+        attempt_date = DateTime.strptime("#{date[:year]} #{date[:month]} #{date[:day]}", "%Y %m %d")
+        params[:attempt][:date] = attempt_date
+      end
+    end
 
+    respond_to do |format|
+      if @attempt.update_attributes(attempt_params)
+        self.set_other_models()
+        format.html { redirect_to @attempt, notice: 'Attempt was successfully updated.' }
+        format.js
+        format.json { render json: @attempt, status: :created, location: @attempt }
+      else
+        format.html { render action: "update" }
+        format.json { render json: @attempt.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def delete
@@ -57,7 +89,10 @@ class AttemptsController < ApplicationController
   end
 
   def destroy
-
+    @attempt = current_user.attempts.find_by_id(params[:id])
+    @climb = @attempt.climb
+    @attempt.destroy
+    self.set_other_models()
   end
 
   private
