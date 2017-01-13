31cb217f64efe475842d91fa178d5649e6bf8a8e
Thomas Murphy <thomas.c.murphy@gmail.com>
2017-01-12 17:29:57 -0700

    better climb delete sorting

--- app/controllers/climbs_controller.rb
+++ app/controllers/climbs_controller.rb
@@ -1,30 +1,8 @@
 class ClimbsController < ApplicationController
-  helper_method :sort_by, :sort_direction
+  helper_method :sort_by, :sort_direction, :page, :per_page, :ordered_climbs
 
   def index
-    if sort_by == "redpoint_date"
-      require 'will_paginate/array'
-      @climbs = current_user.climbs.sort_by{|c| [c.redpointed ? 0 : 1, c.redpoint_date, c.created_at]}
-      if sort_direction.downcase == "desc"
-        @climbs = @climbs.reverse
-      end
-    else
-      @climbs = current_user.climbs.order("#{sort_by} #{sort_direction}", "id ASC")
-    end
-
-    if params[:climb_type].present?
-      @climbs = @climbs.where(climb_type: params[:climb_type])
-    end
-
-    if params[:outdoor].present?
-      @climbs = @climbs.where(outdoor: params[:outdoor] == "true")
-    end
-
-    #@climbs = @climbs.sort_by{|c| [c.redpointed ? 0 : 1, c.redpoint_date, c.created_at]}.reverse
-
-    page = (params[:page] || 1).to_i
-    per_page = (params[:per_page] || 20).to_i
-    @climbs = @climbs.paginate(:page => page, :per_page => per_page)
+    @climbs = ordered_climbs
 
     respond_to do |format|
       format.html
@@ -132,7 +110,7 @@ class ClimbsController < ApplicationController
   end
 
   def update
-    @climbs = current_user.climbs.order(created_at: :desc)
+    @climbs = ordered_climbs
     @climb = current_user.climbs.find_by_id(params[:id])
     respond_to do |format|
       if @climb.update_attributes(climb_params)
@@ -158,10 +136,9 @@ class ClimbsController < ApplicationController
   end
 
   def destroy
-    @climbs = current_user.climbs.order(created_at: :desc)
     @climb = current_user.climbs.find_by_id(params[:id])
     @climb.destroy
-    @climbs = @climbs.sort_by{|c| [c.redpointed ? 0 : 1, c.redpoint_date]}.reverse
+    @climbs = ordered_climbs
   end
 
 
@@ -193,5 +170,38 @@ class ClimbsController < ApplicationController
 
     def sort_direction
       %w[asc desc].include?(params[:sort_direction]) ? params[:sort_direction] : "desc"
+    end
+
+    def page
+      (params[:page] || 1).to_i
+    end
+
+    def per_page
+      (params[:per_page] || 20).to_i
+    end
+
+    def ordered_climbs
+      if sort_by == "redpoint_date"
+        require 'will_paginate/array'
+        climbs = current_user.climbs.sort_by{|c| [c.redpointed ? 0 : 1, c.redpoint_date, c.created_at]}
+        if sort_direction.downcase == "desc"
+          climbs = climbs.reverse
+        end
+      else
+        climbs = current_user.climbs.order("#{sort_by} #{sort_direction}", "id ASC")
+      end
+
+      if params[:climb_type].present?
+        climbs = climbs.where(climb_type: params[:climb_type])
+      end
+
+      if params[:outdoor].present?
+        climbs = climbs.where(outdoor: params[:outdoor] == "true")
+      end
+
+      #@climbs = @climbs.sort_by{|c| [c.redpointed ? 0 : 1, c.redpoint_date, c.created_at]}.reverse
+
+      climbs = climbs.paginate(:page => page, :per_page => per_page)
+      climbs
     end
 end
