1e4e7ea42763291444f9f36884b304fbf396e64e
Thomas Murphy <thomas.c.murphy@gmail.com>
2017-01-12 17:13:08 -0700

    fixing again dummy

--- db/migrate/20170105175227_create_messages.rb
+++ db/migrate/20170105175227_create_messages.rb
@@ -2,7 +2,7 @@ class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
       t.references :user, index: true, foreign_key: true
-      t.references :parent_message_id, index: true, foreign_key: true
+      t.integer :parent_message_id, index: true, foreign_key: true
       t.text :body
       t.string :title
       t.references :messageable, polymorphic: true, index: true
