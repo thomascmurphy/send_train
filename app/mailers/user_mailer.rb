42d67164a1a42b760431b4f5fcd440681508b3d9
Thomas Murphy <thomas.c.murphy@gmail.com>
2018-04-17 09:13:48 -0600

    Update user_mailer.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
 
   def comment_email(subject, body, from_email)
-    mail(to: "thomas.c.murphy+send_train@gmail.com", subject: subject, body: "body from: #{from_email}", from: "thomas.c.murphy+send_train@gmail.com")
+    mail(to: "thomas.c.murphy+send_train@gmail.com", subject: subject, body: "#{body} from: #{from_email}", from: "thomas.c.murphy+send_train@gmail.com")
   end
 
   def share_email(item_share)
